158eb13394c7d81cd7950a2f6d651da4972e27d6
Principe Cabrera <pcabrera@Galactus.local>
2020-06-07 19:57:42 -0400

    Updates

---
+++ precache-manifest.31fdf6b4750c07e1dc3fe2560b9ab521.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc517b7d3938cbca3a21ffd650412511",
+    "url": "/monsbook/index.html"
+  },
+  {
+    "revision": "93d96115fe466daa7977",
+    "url": "/monsbook/static/css/main.1288337e.chunk.css"
+  },
+  {
+    "revision": "b70dc66256ce65c4cdbb",
+    "url": "/monsbook/static/js/2.2363b477.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsbook/static/js/2.2363b477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d96115fe466daa7977",
+    "url": "/monsbook/static/js/main.358700e6.chunk.js"
+  },
+  {
+    "revision": "39f43f8d98cf59a871da",
+    "url": "/monsbook/static/js/runtime-main.8be5056a.js"
+  }
+]);
